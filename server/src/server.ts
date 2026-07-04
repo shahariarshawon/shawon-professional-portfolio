49b06fb0dcce264fe2f36fa2c07e60fc0a879b8c
shahariarshawon <shahariararafat7739@gmail.com>
2026-07-04 10:06:21 +0600

    updated the server.ts with unhandled rejection detection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,20 @@
-import "dotenv/config";
 import app from "./app";
+import { env } from "./config/env";
 
-const PORT = process.env.PORT || 5000;
+const server = app.listen(env.port, () => {
+  console.log(`Server is running on http://localhost:${env.port}`);
+});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled Rejection detected:", reason);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception detected:", error);
+
+  process.exit(1);
 });

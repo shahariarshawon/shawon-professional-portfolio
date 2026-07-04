459b9d3d301887fb6cd5fbeee6c5a4e732c0b76d
shahariarshawon <shahariararafat7739@gmail.com>
2026-07-04 11:12:27 +0600

    Solved prisma client error by adding prisma adapterpg

--- server/src/utils/prisma.ts
+++ server/src/utils/prisma.ts
@@ -1,8 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "../config/env";
+
+const adapter = new PrismaPg({
+  connectionString: env.databaseUrl
+});
 
 const prisma = new PrismaClient({
+  adapter,
   log:
-    process.env.NODE_ENV === "development"
+    env.nodeEnv === "development"
       ? ["query", "error", "warn"]
       : ["error"]
 });

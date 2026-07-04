eefdb7b0f9280c31d1d1658d226ebdd5f2c2cb56
shahariarshawon <shahariararafat7739@gmail.com>
2026-07-05 00:19:20 +0600

    added public route into the main routes list

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,23 +1,28 @@
 import { Router } from "express";
 import databaseRoutes from "./database.route";
 import healthRoutes from "./health.route";
-import authRoutes from "../modules/auth/auth.route"
+import authRoutes from "../modules/auth/auth.route";
+import publicRoutes from "../modules/public/public.route";
 
 const router = Router();
 
 const moduleRoutes = [
   {
     path: "/health",
-    route: healthRoutes
+    route: healthRoutes,
   },
   {
     path: "/database",
-    route: databaseRoutes
+    route: databaseRoutes,
   },
   {
-    path:"/auth",
-    route: authRoutes
-  }
+    path: "/auth",
+    route: authRoutes,
+  },
+  {
+    path: "/public",
+    route: publicRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => {

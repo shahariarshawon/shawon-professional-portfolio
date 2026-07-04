12022cf25ef285433ce8105920c750039669e939
shahariarshawon <shahariararafat7739@gmail.com>
2026-07-04 10:57:42 +0600

    added prisma.ts and updated the server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import { env } from "./config/env";
+import prisma from "./utils/prisma";
 
 const server = app.listen(env.port, () => {
   console.log(`Server is running on http://localhost:${env.port}`);
@@ -8,13 +9,24 @@ const server = app.listen(env.port, () => {
 process.on("unhandledRejection", (reason) => {
   console.error("Unhandled Rejection detected:", reason);
 
-  server.close(() => {
+  server.close(async () => {
+    await prisma.$disconnect();
     process.exit(1);
   });
 });
 
-process.on("uncaughtException", (error) => {
+process.on("uncaughtException", async (error) => {
   console.error("Uncaught Exception detected:", error);
 
+  await prisma.$disconnect();
   process.exit(1);
+});
+
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM received. Closing server...");
+
+  server.close(async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  });
 });

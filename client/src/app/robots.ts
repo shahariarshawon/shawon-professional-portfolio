30c6271188b32ddc8270a961be4bb8a0c5aa989d
shahariarshawon <shahariararafat7739@gmail.com>
2026-07-05 11:22:03 +0600

    added robots

---
+++ client/src/app/robots.ts
@@ -0,0 +1,17 @@
+import type { MetadataRoute } from "next";
+
+import { siteConfig } from "@/constants/site";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin", "/admin/*", "/api/*"]
+      }
+    ],
+    sitemap: `${siteConfig.url}/sitemap.xml`,
+    host: siteConfig.url
+  };
+}

6afb16af9f5e619d0af70929eb620405bac7678c
shahariarshawon <shahariararafat7739@gmail.com>
2026-07-05 11:41:25 +0600

    updated api timeout

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
+
 import { siteConfig } from "@/constants/site";
 
 export const api = axios.create({
   baseURL: siteConfig.apiUrl,
   withCredentials: true,
+  timeout: 15000,
   headers: {
     "Content-Type": "application/json"
   }
